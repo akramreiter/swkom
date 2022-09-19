07a06c00a06ce81acf2e0fbe20ab3e933d48c985
akramreiter <a.kramreiter@gmx.at>
2022-09-19 13:19:00 +0200

    removed comment and broken test file

--- src/main/java/at/fhtw/swen3/OpenApiGeneratorApplication.java
+++ src/main/java/at/fhtw/swen3/OpenApiGeneratorApplication.java
@@ -12,12 +12,6 @@ import org.springframework.context.annotation.ComponentScan;
 public class OpenApiGeneratorApplication {
 
     public static void main(String[] args) {
-        /*
-        Starts the application
-        You probably saw that one coming
-        It's nothing special
-        and definitely not worth writing this much of a comment for
-         */
         SpringApplication.run(OpenApiGeneratorApplication.class, args);
     }
 
